a5705d3a5bcd255dc53e6b19872463d493001e3e
Sagar Embadi <sagarembadi7@gmail.com>
2025-01-22 12:36:25 +0530

    Adding Project Files

--- JavaScript/RED_BUS/signUp/signup.js
+++ JavaScript/RED_BUS/signUp/signup.js
@@ -5,16 +5,16 @@ form.addEventListener("submit",(s)=>{
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirm-password').value;
+
     if (password !== confirmPassword) {
       alert('Passwords do not match!');
       return;
     }
-
     let users = JSON.parse(localStorage.getItem('users')) || [];
-    // if (users.find(user => user.email === email)) {
-    //   alert('email already exists!');
-    //   return;
-    // }
+    if (users.find(user => user.email === email)) {
+      alert('email already exists!');
+      return;
+    }
     users.push({ email, password });
     localStorage.setItem('users', JSON.stringify(users));
     alert('Sign up successful!');
