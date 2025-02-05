0dfe73739215bf85335cefd24e74616b583914ae
Sagar Embadi <sagarembadi7@gmail.com>
2025-02-06 00:33:05 +0530

    updating buses page file

--- JavaScript/RED_BUS/buses/buses.js
+++ JavaScript/RED_BUS/buses/buses.js
@@ -26,12 +26,45 @@ function buses(type=null){
         let card=document.createElement("div")
         card.className="card"
         card.innerHTML=`
-        <div>${x.operator}<br><br>${x.type}</div>
-        <div>${x.arrival_time}<br><br>${busData.origin.toUpperCase()}</div>
-        <div>${x.departure_time}<br><br>${busData.destination.toUpperCase()}</div>
-        <div>${x.price}<br><br><button id="booking">BOOK NOW</button></div>
-        `
+        <div class="busDetails">
+            <div class="operatorDetails">
+                <h3>${x.operator}</h3>
+                <p>${x.type}</p>
+            </div>
+            <div class="timeDetails">
+                <p>Departure : &emsp14;&emsp14;<span>${x.arrival_time}</span></p>
+                <p>Arrival : &emsp14;&emsp14;&emsp14;&emsp14; <span>${x.departure_time}</span></p>
+            </div>
+            <div class="routeDetails">
+                <p>${busData.origin.toUpperCase()}</p>
+                <p>${busData.destination.toUpperCase()}</p>
+            </div>
+            <div class="rating_pricing">
+                <div class="rating">
+                    <div class="rating_star">⭐ <span id="rate"></span></div>
+                    <div class="pass"><span id="pass"></span> Passengers</div>
+                </div>
+                <div class="fare-section">
+                    <div class="fare">INR <span>${x.price}</span></div>
+                    <div class="seat-availability">Seats available</div>
+                </div>
+            </div>
+        </div>
+        <div class="icons_btn">
+            <div class="icons">
+                <i class="fa-solid fa-bottle-water"></i>
+                <i class="fa-solid fa-plug-circle-bolt"></i>
+                <i class="fa-solid fa-ticket"></i>
+                <i class="fa-solid fa-bus-simple"></i>
+                <i class="fa-solid fa-phone"></i>
+                <i class="fa-solid fa-kit-medical"></i>
+            </div>
+            <button id="booking">View Seats</button>
+        </div>`
         busList.appendChild(card)
+        card.querySelector("#rate").innerText=`${Math.floor((Math.random() *(5 - 2) + 2)*10)/10}`
+        card.querySelector("#pass").innerText=`${Math.floor((Math.random() *10))}`
+
         card.querySelector("#booking").addEventListener("click",()=>{
             location.href="../booking/booking.html"
             localStorage.setItem("seatInfo",JSON.stringify(x))
