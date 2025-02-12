29ad34f9ab440f0b7b1d39fc74e6f0f1b526f283
Sagar Embadi <sagarembadi7@gmail.com>
2025-02-13 01:25:26 +0530

    updating ticket page with media queries

--- JavaScript/RED_BUS/ticket/ticket.js
+++ JavaScript/RED_BUS/ticket/ticket.js
@@ -1,21 +1,18 @@
-// import { date } from "../home/home.js";
-// console.log(date);
-
-// import { today } from "../home/home.js"
-// Set the input's default value to today's date
-const today = new Date().toISOString().split('T')[0];
-document.getElementById('date').innerText= `${today}`
-
 let seatInfo = JSON.parse(localStorage.getItem("seatInfo"))
 let busData = JSON.parse(localStorage.getItem("busData"))
 let passengersInfo = JSON.parse(localStorage.getItem("passengersInfo"))
+let routeDetails = JSON.parse(localStorage.getItem("routeDetails"))
+let price = localStorage.getItem("price")
 document.getElementById("pickup").innerText=`${busData.origin}`
 document.getElementById("drop").innerText=`${busData.destination}`
+document.getElementById('date').innerText=`${routeDetails.date}`
 document.getElementById("operator").innerText=`${seatInfo.operator}`
 document.getElementById("type").innerText=`${seatInfo.type}`
 document.getElementById("busNo").innerText=`${seatInfo.bus_id}`
 document.getElementById("arrival_time").innerText=`${seatInfo.arrival_time}`
 document.getElementById("departure_time").innerText=`${seatInfo.departure_time}`
+document.getElementById("price").innerText=`${price}`
+
 
 let passDiv = document.getElementById("passengers_list")
 passengersInfo.forEach(x => {
